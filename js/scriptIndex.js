c30a8586820d43c9603567e76fc95c62f0af17f2
Jason GUITTARD <jason.guittard2025@campus-eni.fr>
2025-12-19 10:00:03 +0100

    Itération 3 - Tâche 3.1: Page Acceuil | Showing details modal when detail button is clicked

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -4,6 +4,7 @@ let gridDiv = document.getElementById('gridDisplay')
 let gridDisplayDiv = document.getElementById("gridDisplayDiv")
 let tableDiv = document.getElementById('tableDisplay')
 let apprenantTable = document.getElementById("apprenantsTable")
+let modalDiv = document.getElementById('detailModal')
 
 function init() {
 	document.getElementById("table").addEventListener('change', updateDisplayTable)
@@ -26,7 +27,7 @@ function init() {
 }
 
 function updateTable(data) {
-
+	let attributToAdd = [["data-bs-toggle", "modal"], ["data-bs-target", "#detailModal"]]
 
 	data.forEach(element => {
 		const clone = document.getElementById("templateListeApprenants").content.cloneNode(true)
@@ -34,12 +35,56 @@ function updateTable(data) {
 		clone.querySelector('td[name]').innerText = element.name
 		clone.querySelector('td[lastname]').innerText = element.lastname
 		clone.querySelector('td[city]').innerText = element.city
+		clone.querySelector('td[detail]').firstElementChild.id = element.id
+
+		attributToAdd.forEach(element => {
+			clone.querySelector('td[detail]').firstElementChild.setAttribute(element[0], element[1])
+		})
+
+		clone.querySelector('td[detail]').firstElementChild.addEventListener("click", setInformation)
 
 		apprenantTable.appendChild(clone)
 	});
 }
 
+async function setInformation(e) {
+	let id = e.target.id
+	let lastnameElement = document.querySelector("h3[lastname]")
+	let nameElement = document.querySelector("h3[name]")
+	let cityElement = document.querySelector("h3[city]")
+	let storydiv = document.querySelector("div[story]")
+	let avatarElement = document.querySelector("img[avatar]")
+
+	let data = await fetch('../json/promo.json').then(response => response.json())
+	let apprenantData = data.apprenants.find(element => element.id === parseInt(id))
+
+	lastnameElement.innerText = apprenantData.lastname ? apprenantData.lastname : "Non renseigné"
+	nameElement.innerText = apprenantData.name ? apprenantData.name : "Non renseigné"
+	cityElement.innerText = apprenantData.city ? apprenantData.city : "Non renseigné"
+
+	storydiv.innerHTML = ""
+	avatarElement.setAttribute("src", "assets/Images/Avatar/avatar.webp")
+	
+	if(apprenantData.story.length > 0) {
+		apprenantData.story.forEach(story => {
+			let paragraph = document.createElement("p")
+			paragraph.className = "my-1"
+			paragraph.innerText = story
+			storydiv.appendChild(paragraph)
+		})
+	} else {
+		let paragraph = document.createElement("p")
+		paragraph.innerText = "Aucune anecdotes renseigné"
+		paragraph.className = "my-1"
+		storydiv.appendChild(paragraph)
+	}
+
+	if(apprenantData.avatar && apprenantData.avatar.length > 0)
+		avatarElement.setAttribute("src", apprenantData.avatar)
+}
+
 function updateGrid(data) {
+	let attributToAdd = [["data-bs-toggle", "modal"], ["data-bs-target", "#detailModal"]]
 
 	data.forEach(element => {
 		const clone = document.getElementById("templateGridApprenants").content.cloneNode(true)
@@ -57,6 +102,13 @@ function updateGrid(data) {
 			clone.querySelector('p').innerText = element.story[Math.floor(Math.random() * element.story.length)]
 		else
 			clone.querySelector('p').innerText = "Description du profil"
+
+		attributToAdd.forEach(element => {
+			clone.querySelector('a[detail]').setAttribute(element[0], element[1])
+		})
+
+		clone.querySelector('a[detail]').id = element.id
+		clone.querySelector('a[detail]').addEventListener("click", setInformation)
 
 		gridDiv.appendChild(clone)
 	});
